5259ac5bfb57e435f660d5d6997c6bd51b76119e
Mohamed Tedjani Meftah <mohamed-ted@live.com>
2016-11-03 16:57:58 +0100

    Plugin concept working

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const postcss = require('postcss');
  
 module.exports = postcss.plugin('postcss-precision', options => {
     options = options || {}
-    options.units = options.units || '%|em|px'
+    options.units = options.units || '%|em|px|rem'
     options.precision = options.precision || 2
-    const isFloat = new RegExp(`(\d+?\.\d{${options.precision},})(${options.units})`)
+    const isFloat = new RegExp(`(\\d+?\\.\\d{${options.precision},})(${options.units})`, 'gi')
     const precision = Math.pow(10,3)
     return function (css) {
-        css.walkDecls(decl => {
-            if (!decl.value || isFloat.test(decl.value)) {
+       
+        css.each(decl => {
+            console.log(decl)
+            if (decl.value && isFloat.test(decl.value)) {
+                console.log(decl.value)
                 const value = isFloat.exec(decl.value)[1]
                 const rounded = Math.round(parseFloat(value) * precision) / precision
                 decl.value = decl.value.replace(value, rounded.toString())
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 const postcss = require('postcss')
 const path = require('path')
+const precision = require('./../index')
 
-postcss([ require('./../index')])
+postcss([precision()])
     .process(fs.readFileSync(path.join(__dirname, './input.css')))
     .then(function (result) { fs.writeFileSync(path.join(__dirname, './output.css'), result.css) })
