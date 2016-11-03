2471f55cb097c5e5b3ba1aa4f1b788d71faebc2b
Mohamed Tedjani Meftah <mohamed-ted@live.com>
2016-11-03 17:52:01 +0100

    Test multiple values

--- tests/test.js
+++ tests/test.js
@@ -3,6 +3,6 @@ const postcss = require('postcss')
 const path = require('path')
 const precision = require('./../index')
 
-postcss([precision()])
+postcss([precision({units: "px|em"})])
     .process(fs.readFileSync(path.join(__dirname, './input.css')))
-    .then(function (result) { fs.writeFileSync(path.join(__dirname, './output.css'), result.css) })
+    .then(function (result) { console.log(result.css); fs.writeFileSync(path.join(__dirname, './output.css'), result.css) })
