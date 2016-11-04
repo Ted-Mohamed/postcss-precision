df04e26da32c47b26cc6608d044aa2966bffbc63
Mohamed Tedjani Meftah <mohamed-ted@live.com>
2016-11-04 09:47:09 +0100

    Problem with RegExp

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const postcss = require('postcss');
- 
+
 module.exports = postcss.plugin('postcss-precision', options => {
     options = options || {}
     options.units = options.units || '%|em|px|rem'
     options.precision = options.precision || 3
-    const isFloat = new RegExp(`(\\d+?\\.\\d{${options.precision},})(${options.units})`, 'gi')
-    const precision = Math.pow(10,3)
+    const isFloat = new RegExp(`(\-?\\d{0,}?\\.\\d{${ options.precision},})*?`, 'gi')
+    const precision = Math.pow(10, 3)
+    console.log(isFloat)
 
-    
     return function (css) {
-        css.walkRules(function(rule) {
-            rule.walkDecls(decl => {
+        css.walkRules(function (rule) {
+            rule.walkDecls(function (decl, i) {
+                console.log(decl.value, isFloat.exec(decl.value))
                 if (decl.value && isFloat.test(decl.value)) {
-                    console.log(decl.value)
-                    const value = isFloat.exec(decl.value)[1]
-                    const rounded = Math.round(parseFloat(value) * precision) / precision
-                    rule.insertBefore(decl, { prop: decl.prop, value: decl.value.replace(value, rounded.toString()) })
                 }
             })
         })
+        // css.walkRules(function(rule) {
+        //     rule.walkDecls(decl => {
+        //         if (decl.value && isFloat.test(decl.value)) {
+        //             console.log(decl.value)
+        //             const value = isFloat.exec(decl.value)[1]
+        //             const rounded = Math.round(parseFloat(value) * precision) / precision
+        //             rule.insertBefore(decl, { prop: decl.prop, value: decl.value.replace(value, rounded.toString()) })
+        //         }
+        //     })
+        // })
     }
 })
--- tests/test.js
+++ tests/test.js
@@ -3,6 +3,6 @@ const postcss = require('postcss')
 const path = require('path')
 const precision = require('./../index')
 
-postcss([precision({units: "px|em"})])
+postcss([precision()])
     .process(fs.readFileSync(path.join(__dirname, './input.css')))
     .then(function (result) { console.log(result.css); fs.writeFileSync(path.join(__dirname, './output.css'), result.css) })
