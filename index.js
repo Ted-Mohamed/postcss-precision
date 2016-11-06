9a70b8b304b2af87ba5881b79089ac06e1f70935
Mohamed Tedjani Meftah <mohamed-ted@live.com>
2016-11-06 22:45:40 +0100

    Improvement on the RegExp bug

--- index.js
+++ index.js
@@ -4,27 +4,24 @@ module.exports = postcss.plugin('postcss-precision', options => {
     options = options || {}
     options.units = options.units || '%|em|px|rem'
     options.precision = options.precision || 3
-    const isFloat = new RegExp(`(\-?\\d{0,}?\\.\\d{${ options.precision},})*?`, 'gi')
-    const precision = Math.pow(10, 3)
-    console.log(isFloat)
-
+    
+    const isFloat = new RegExp(`([-+]?\\d*\\.\\d{${options.precision},})`, 'gi')
+    const precision = Math.pow(10, options.precision)
     return function (css) {
         css.walkRules(function (rule) {
             rule.walkDecls(function (decl, i) {
-                console.log(decl.value, isFloat.exec(decl.value))
-                if (decl.value && isFloat.test(decl.value)) {
+                if (decl.value) {
+                    let value = decl.value
+                    let matches
+                    console.log(value)
+                    while ((matches = isFloat.exec(value)) !== null) {
+                        const rounded = Math.round(parseFloat(matches[1]) * precision) / precision
+                        console.log(rounded)
+                        value.replace(matches[1], rounded.toString())
+                    }
+                    rule.insertBefore(decl, { prop: decl.prop, value: value })
                 }
             })
         })
-        // css.walkRules(function(rule) {
-        //     rule.walkDecls(decl => {
-        //         if (decl.value && isFloat.test(decl.value)) {
-        //             console.log(decl.value)
-        //             const value = isFloat.exec(decl.value)[1]
-        //             const rounded = Math.round(parseFloat(value) * precision) / precision
-        //             rule.insertBefore(decl, { prop: decl.prop, value: decl.value.replace(value, rounded.toString()) })
-        //         }
-        //     })
-        // })
     }
 })
